e1b447623c2b8c18ad2b068c26cf02ba87d644a2
pengju.lou <pengju.lou@weimob.com>
2022-04-07 19:31:46 +0800

    craco

--- craco.config.js
+++ craco.config.js
@@ -1,11 +1,18 @@
-const { when, whenDev, whenProd, whenTest, ESLINT_MODES, POSTCSS_MODES } = require("@craco/craco");
+const {
+  when,
+  whenDev,
+  whenProd,
+  whenTest,
+  ESLINT_MODES,
+  POSTCSS_MODES,
+} = require('@craco/craco');
 
 module.exports = {
   webpack: {
     configure: {
       output: {
         publicPath: '/tinymce2docx',
-      }
-    }
-  }
+      },
+    },
+  },
 };
